5623dc6e28ffdfb688aea03455cf05bacc4bf5f5
phenonymous <29523105+phenonymous@users.noreply.github.com>
2018-11-09 21:52:16 +0100

    POSIX support

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 ## shell-progressbar
 An asynchronous progressbar for bash shell scripts inspired by APT
+This version is POSIX compliant and should work in most shells
 
 ![bar](https://raw.githubusercontent.com/phenonymous/shell-progressbar/master/images/progressbar.gif)
 
@@ -16,50 +17,53 @@ or by using wget
 . <(wget -qO- "https://git.io/progressbar")
 ```
 
+Mac users can use the following instead, since sourcing directly from curl does not work
+```sh
+eval "$(curl -sLo- "https://git.io/progressbar")"
+```
+
+You could also clone this repo and source it locally
+
 ### Usage:
 
 Make a call to
 
 ```sh
-bar::start
+bar__start
 ```
 before any progress should be reported. This will setup the status line by shrinking the terminal scroll area by one row.
 Then determine the total steps to be reported - either you're using this in a loop or do manual reporting in your script. What ever way suits your needs, make a call to
 
 ```sh
-bar::status_changed <steps done> <total steps>
+bar__status_changed <steps done> <total steps>
 ```
 whenever progress is made. This function will then determine if the status line should be updated.
 
 Finally make a call to
 
 ```sh
-bar::stop
+bar__stop
 ```
 when you're done and this function will restore the terminal size.
 
 #### Example
 
 ```sh
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
-. <(curl -sLo- "https://git.io/progressbar")
+eval "$(wget -qO- "https://git.io/progressbar")"
 
-bar::start
+bar__start
 
-StuffToDo=("Stuff1" "Stuff2" "Stuff3")
-
-TotalSteps=${#StuffToDo[@]}
-
-for Stuff in ${StuffToDo[@]}; do
+for i in $(seq 10); do
   # Do stuff
-  echo "Invoking ${Stuff} to do some stuffs..."
+  echo "Invoking stuff${i} to do some stuffs..."
   StepsDone=$((${StepsDone:-0}+1))
-  bar::status_changed $StepsDone $TotalSteps
+  bar::status_changed $i $TotalSteps
   sleep 1
 done
 
-bar::stop
+bar__stop
 ```
 
 ### Customization:
--- progress.sh
+++ progress.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 #    An asynchronous progress bar inspired by APT PackageManagerFancy Progress
 #    Copyright (C) 2018  Kristoffer Minya
@@ -15,99 +15,59 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>
-
-# The following terms are used for maintainment
-# FIXME  :   Code that needs to improved or does not work
-# DEBUG  :   Code that needs to be debugged
-# TEST   :   Code that needs to be tested for alternatives 
-# TODO   :   Reminder for code that needs to be added or changed
-# FUTURE :   For future changes, this must be considered
-# IDEA   :   Ideas for future improvement or added features
-
-set -aeuo pipefail
-#-- TODO: Write functions for debugging and logging
-#-- TODO: Write unit tests
-#-- IDEA: Use Bash Infinity
-#-- FUTURE: [[ is non-standard, replace with [
-#-- TODO: Port to POSIX - this breaks the above idea
-#-- \___  Associative arrays are non-standard
+set -a
 
 percentage="0.0"
 last_reported_progress=-1
 
 #-- In which rate reporting should be done
-reporting_steps=${reporting_steps:-1}     # reporting_step can be set by the caller, defaults to 1
+reporting_steps=${reporting_steps:-1}       # reporting_step can be set by the user, defaults to 1
 
-foreground="${foreground:-$(tput setaf 0)}" # Foreground can be set by the caller, defaults to black
-background="${background:-$(tput setab 2)}" # Background can be set by the caller, defaults to green
+foreground="${foreground:-$(tput setaf 0)}" # Foreground can be set by the user, defaults to black
+background="${background:-$(tput setab 2)}" # Background can be set by the user, defaults to green
 reset_color="$(tput sgr0)"
 
 #-- Command aliases for readability
-# save_cursor='tput sc'
-# restore_cursor='tput rc'
-# disable_cursor='tput civis'
-# enable_cursor='tput cnorm'
-# scroll_area='tput csr'
-# move_to='tput cup'
-# move_up='tput cuu 2'
-# flush='tput ed'
-
-# Wrap functions to make sure we use the right one
-# ==================================================
-# PATH="/bin:/usr/bin:/sbin:$PATH"
-
-echo() {
-  builtin echo "$@"
-}
-
-printf() {
-  builtin printf "$@"
-}
+save_cursor='tput sc'
+restore_cursor='tput rc'
+disable_cursor='tput civis'
+enable_cursor='tput cnorm'
+scroll_area='tput csr'
+move_to='tput cup'
+move_up='tput cuu'
+flush='tput ed'
 
 
 # Bash does not handle floats
 # This section defines some math functions using awk
 # ==================================================
 
-#-- IDEA: Use large numbers to simulate floating points
-#-- That might not be efficient - research needs to be done
 
-math::floor() {
+math__floor() {
   #-- This function takes a pseudo-floating point as argument
   #-- and rounds down to nearest integer
   awk -v f="$1" 'BEGIN{f=int(f); print f}'
 }
 
-math::ceiling() {
-  #-- This function takes a pseudo-floating point as argument
-  #-- and rounds up to nearest integer
-  awk -v f="$1" 'BEGIN{f=int(f)+1; print f}'
-}
-
-math::round() {
+math__round() {
   #-- This function takes a pseudo-floating point as argument
   #-- and rounds to nearest integer
   awk -v f="$1" 'BEGIN {printf "%.0f\n", f}'
 }
 
-math::min() {
+math__min() {
   #-- Takes two values as arguments and compare them
   awk -v f1="$1" -v f2="$2" 'BEGIN{if (f1<=f2) min=f1; else min=f2; printf min "\n"}'
 }
 
-math::max() {
+math__max() {
   #-- Takes two values as arguments and compare them
   awk -v f1="$1" -v f2="$2" 'BEGIN{if (f1>f2) max=f1; else max=f2; printf max "\n"}'
 }
 
-math::float_multiplication() {
-  #-- Takes two floats and multiply them
-  awk -v f1="$1" -v f2="$2" 'BEGIN{print f1 * f2}'
-}
-
-math::float_division() {
-  #-- Takes two floats and divide them
-  awk -v f1="$1" -v f2="$2" 'BEGIN{print f1 / f2}'
+math__calc() {
+  #-- Normal calculator
+  awk "BEGIN{print $*}"
 }
 
 
@@ -126,35 +86,35 @@ __tty_size(){
 }
 
 __change_scroll_area() {
-  local -i n_rows=$1
+  n_rows=$1
   #-- Return if number of lines is 1
-  if (( n_rows <= 1)); then
+  if [ $n_rows -lt 1 ]; then
     return 1
   fi
 
-  ((n_rows-=2))
+  n_rows=$((n_rows-2))
 
   #-- Go down one line to avoid visual glitch 
   #-- when terminal scroll region shrinks by 1
   echo
 
   #-- Save cursor position
-  tput sc
+  eval "${save_cursor}"
 
   #-- Set scroll region
-  tput csr 0 $n_rows
+  eval "${scroll_area} 0 $n_rows"
 
   #-- Restore cursor
-  tput rc
+  eval "${restore_cursor}"
 
   #-- Move up 1 line in case cursor was saved outside scroll region
-  tput cuu 2
+  eval "${move_up} 2"
   
   echo
 
   #-- Set tty size to reflect changes to scroll region
   #-- this is to avoid i.e pagers to override the progress bar
-  ((++n_rows))
+  n_rows=$((n_rows+1))
 
   #-- Temporarily disabling SIGWINCH to avoid a loop caused by stty sending SIGWINCH whenever theres a change in size
   trap '' WINCH
@@ -163,8 +123,6 @@ __change_scroll_area() {
 }
 
 __status_changed() {
-  local -i StepsDone TotalSteps __int_percentage
-  
   StepsDone=$1
   TotalSteps=$2
   
@@ -172,14 +130,23 @@ __status_changed() {
   #-- Sanity check reporting_steps, if this value is too big no progress will be written
   #-- Should that really be checked here?
 
-  percentage="$(math::float_multiplication $(math::float_division $StepsDone $TotalSteps) "100.00")"
-  
-  __int_percentage=$(math::round "$percentage")
+  percentage=$(math__calc $(math__calc $StepsDone/$TotalSteps)*100.00)
 
-  #-- FUTURE: printf -v is non-standard, for POSIX replace with subshell
-  printf -v progress_str "Progress: [%3li%%]" $__int_percentage
+  __int_percentage=$(math__round "$percentage")
 
-  if (( __int_percentage < (last_reported_progress + reporting_steps) )); then
+  #-- Note: Below string is not POSIX compliant
+  #progress_str="$(printf "Progress: [%3li%%]" $__int_percentage)"
+  it=0
+  gaps=""
+  if [ ${#__int_percentage} -lt 3 ]; then
+    while [ $it -lt $((3-(${#__int_percentage}%3))) ]; do
+      gaps="${gaps} "
+      it=$((it+1))
+    done
+  fi
+  progress_str="Progress: [${gaps}${__int_percentage}%]"
+
+  if [ $__int_percentage -lt $((last_reported_progress + reporting_steps)) ]; then
     return 1
   else
     return 0
@@ -187,121 +154,119 @@ __status_changed() {
 }
 
 __progress_string() {
-  local output Percent
-  local -i OutputSize BarSize BarDone it
-  
   output=""
   Percent="$1"
-  ((OutputSize=$2))
+  OutputSize=$2
 
   #-- Return an empty string if OutputSize is less than 3
-  if ((OutputSize < 3)); then
+  if [ $OutputSize -lt 3 ]; then
     echo "$output"
     return 1
   fi
 
-  ((BarSize=OutputSize-2))
+  BarSize=$((OutputSize-2))
   
-  ((BarDone=$(math::max 0 $(math::min $BarSize $(math::floor $(math::float_multiplication "$Percent" $BarSize))))))
+  BarDone=$(math__max 0 $(math__min $BarSize $(math__floor $(math__calc $Percent*$BarSize))))
   
-  output+="["
-  for (( it = 0; it < BarDone; it++ )); do
-    output+="#"
+  output="${output}["
+  it=0
+  while [ $it -lt $BarDone ]; do
+    output="${output}#"
+    it=$((it+1))
   done
-  for (( it = 0; it < BarSize - BarDone; it++ )); do
-    output+="."
+  it=0
+  while [ $it -lt $((BarSize-BarDone)) ]; do
+    output="${output}."
+    it=$((it+1))
   done
-  output+="]"
+  output="${output}]"
   echo "$output"
   return 0
 }
 
 __draw_status_line(){
   __tty_size
-  if (( HEIGHT < 1 || WIDTH< 1 )); then
+  if [ $HEIGHT -lt 1 ]|| [ $WIDTH -lt 1 ]; then
     return 1
   fi
 
-  local current_percent progress_bar
-  local -i padding __int_percentage progressbar_size
-  ((padding=4))
+  padding=4
   progress_bar=""
 
   #-- Save the cursor
-  tput sc
+  eval "${save_cursor}"
   #-- Make cursor invisible
-  tput civis
+  eval "${disable_cursor}"
 
   #-- Move to last row
-  tput cup $((HEIGHT)) 0
+  eval "${move_to} $((HEIGHT)) 0"
   printf '%s' "${background}${foreground}${progress_str}${reset_color}"
 
-  ((progressbar_size=WIDTH-padding-${#progress_str}))
-  current_percent="$(math::float_division "$percentage" "100.00")"
+  progressbar_size=$((WIDTH-padding-${#progress_str}))
+  current_percent="$(math__calc "$percentage/100.00")"
   
-  progress_bar="$(__progress_string "${current_percent}" ${progressbar_size})"
+  progress_bar="$(__progress_string ${current_percent} ${progressbar_size})"
 
   printf '%s' " ${progress_bar} "
 
   #-- Restore the cursor
-  tput rc
-  tput cnorm
+  eval "${restore_cursor}"
+  eval "${enable_cursor}"
 
-  ((last_reported_progress=$(math::round "$percentage")))
+  last_reported_progress=$(math__round $percentage)
 
   return 0
 }
 
-bar::start() {
-  #-- TODO: Track process that called this function
-  # proc...
+bar__start() {
   E_START_INVOKED=-1
   __tty_size
   __change_scroll_area $HEIGHT
 }
 
-bar::stop() {
+bar__stop() {
   E_STOP_INVOKED=-1
-  if (( ! ${E_START_INVOKED:-0} )); then
-    echo "Warn: bar::stop called but bar::start was not invoked" >&2 
+  if [ ! ${E_START_INVOKED:-0} -lt 0 ]; then
+    echo "Warn: bar__stop called but bar__start was not invoked" >&2 
     echo "Returning.." # Exit or return?
     return 1
   fi
-  #-- Reset bar::start check
+  #-- Reset bar__start check
   E_STOP_INVOKED=0
 
   __tty_size
-  if ((HEIGHT > 0)); then
+  if [ $HEIGHT -gt 0 ]; then
     #-- Passing +2 here because we changed tty size to 1 less than it actually is
     __change_scroll_area $((HEIGHT+2))
 
-    #-- tput ed might fail in which case we force clear
-    trap 'printf "\033[J"' ERR
+    #-- tput ed might fail (OS X) in which case we force clear
+    #-- POSIX sh don't specify ERR, this is a work around
+    set -e
+    trap 'printf "\033[J"; trap handle_exit EXIT HUP INT QUIT PIPE TERM' EXIT
+    set +e
 
     #-- Flush progress bar
-    tput ed
-   
-    trap - ERR
+    eval "${flush}"
+
     #-- Go up one row after flush
     echo
-    tput cuu1
+    eval "${move_up} 1"
   fi
   #-- Restore original (if any) handler
   trap - WINCH
   return 0
 }
 
-#-- FIXME: Pass worker pid?
-bar::status_changed() {
-  if (( ! ${E_START_INVOKED:-0} )); then
-    echo "ERR: bar::start not called" >&2
+
+bar__status_changed() {
+  if [ ! ${E_START_INVOKED:-0} -lt 0 ]; then
+    echo "ERR: bar__start not called" >&2
     echo "Exiting.."
     exit 1
   fi
-  local -i StepsDone TotalSteps
 
-  ((StepsDone=$1))
-  ((TotalSteps=$2))
+  StepsDone=$1
+  TotalSteps=$2
 
   if ! __status_changed $StepsDone $TotalSteps; then
     return 1
@@ -322,22 +287,18 @@ bar::status_changed() {
 
 handle_sigwinch(){
   __tty_size
-  local -i n_rows
-  ((n_rows=HEIGHT))
+  n_rows=$HEIGHT
   __change_scroll_area $n_rows
   __draw_status_line
 }
 
 handle_exit(){
   #-- if stop_exit doesn't have value it means it wasn't invoked
-  (( ! ${E_STOP_INVOKED:-0} )) && bar::stop
+  if [ ! ${E_STOP_INVOKED:-0} -lt 0 ]; then 
+    bar__stop
+  fi
   trap - EXIT
 }
-
-#-- TODO: Trap this with SIGCHLD and let child fire this signal to parent
-# handle_worker_done(){
-#   echo 
-# }
 
 
 ####################################################
